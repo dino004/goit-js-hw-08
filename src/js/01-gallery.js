87576122000701dea127092cef4e96518d30820f
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-04-03 22:34:58 +0300

    Виконав перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,37 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
+const gallery = document.querySelector(".gallery");
+
+const listItemsGallery = getGalleryItemImage(galleryItems);
+
+gallery.innerHTML = listItemsGallery;
+
+function getGalleryItemImage(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+  captionPosition: 'bottom',
+});
